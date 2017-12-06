9b4a69c755a408b4cfa8775e081fef7a547f2fa0
Sangwon Lee <leee.sangwon@gmail.com>
2017-12-07 01:09:46 +0900

    ADD docstring of preprocess_dataset.py

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,3 +1,12 @@
+"""
+Preprocess data.
+    1. Remove image files which don't have matched xml files.
+    2. Convert bmp files to jpg files.
+    3. Change ext information of image in xml file from bmp to jpg.
+Args:
+    dirname:
+        Directory name which include data.
+"""
 import sys
 
 from preprocess_utils import *
